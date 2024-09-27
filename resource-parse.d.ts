f1fe60482ffc7346373d2ef4f0fb9e42a64fd095
Zander Lim <86615446+jyc-start@users.noreply.github.com>
2024-09-27 16:34:20 +0800

    Create resource-parse.d.ts

---
+++ resource-parse.d.ts
@@ -0,0 +1,13 @@
+declare module 'resource_parse' {
+  export function parseResource(urlStr: string， key: string): ParseData;
+
+  export class ParseData {
+    scheme: string;
+    addrs: string[];
+    user: string | null;
+    password: string | null;
+    uri: string;
+    params: Record<string, string>;
+    getAddrInline(): string;
+  }
+}
